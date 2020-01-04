6922509f5c5b7416c2b40523fdff2cd185a9d1be
the_paper__ <the.paper@live.co.kr>
2020-01-05 02:23:34 +0900

    일단 여기까지 세팅

--- zanas.py
+++ zanas.py
@@ -3,12 +3,26 @@ import asyncio
 import sys
 import datetime
 import random
+import os
 
 token = 'token'
 
 for argIdx, argVal in enumerate(sys.argv):
     if argVal == '-token':
         token = sys.argv[argIdx + 1]
+
+
+class GuildData:
+    id = 0
+    tzinfo = None
+
+    channel_id = 0
+
+    def __init__(self, id):
+        self.id = id
+        self.tzinfo = datetime.timezone(datetime.timedelta(hours=0))
+
+    
 
 
 class ZanasClient(discord.Client):
@@ -26,6 +40,11 @@ class ZanasClient(discord.Client):
     
     async def on_ready(self):
         print(f'on_ready: {self.user.name}({self.user.id})')
+        for guild in self.guilds:
+            if guild.id not in self.guildDatas:
+                self.guildDatas[guild.id] = GuildData(guild.id)
+        print(f'guild data count: {len(self.guildDatas)}')
+
 
     # async def on_member_join(self, member):
     #     print(f'on_member_join: {member.display_name}({member.id})')
@@ -41,6 +60,8 @@ class ZanasClient(discord.Client):
 
     async def on_guild_join(self, guild):
         print(f'on_guild_join: {guild.name}({guild.id})')
+        if guild.id not in self.guildDatas:
+            self.guildDatas[guild.id] = GuildData(guild.id)
 
     async def on_guild_remove(self, guild):
         print(f'on_guild_remove: {guild.name}({guild.id})')
@@ -60,12 +81,31 @@ class ZanasClient(discord.Client):
                     print(f'channel: {message.channel.name}({message.channel.id})')
                     print(f'guild: {message.guild.name}({message.guild.id})')
                     await message.channel.send('print debug.')
-                elif args[0] == './자나스':
-                    await message.channel.send('계시자.. 이 목소리가 들린다면 나를 찾아와줘..')
+                elif args[0] == './채팅자나스':
+                    self.guildDatas[message.guild.id].channel_id = message.channel.id
+                    await message.channel.send(f'<#{message.channel.id}> 채널에 알림 등록.')
             
     async def my_background_task(self):
         await self.wait_until_ready()
+        path = 'C:\\Nexon\\TreeOfSavior\\release\\screenshot\\'
         while not self.is_closed():
+            for filename in os.listdir(path):
+                if filename.startswith('check_recchat_'):
+                    f = open(path + filename, 'r', encoding='UTF8')
+                    lines = f.readlines()
+                    for line in lines:
+                        splited = line.split(' ')
+                        text = line.split(splited[2])[1]
+                        print(text[:-1])
+                        if splited[2] == '[Notice]':
+                            for guildKey in self.guildDatas:
+                                if self.guildDatas[guildKey].channel_id > 0:
+                                    await client.get_channel(self.guildDatas[guildKey].channel_id).send(text[:-1])
+
+                    f.close()
+                    os.remove(path + filename)
+                elif filename.startswith('recchat_'):
+                    os.rename(path + filename, path + 'check_' + filename)
             await asyncio.sleep(1)
 
 
